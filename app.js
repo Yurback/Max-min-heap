84ca75ad9b8594c9c939e10989cf90cd2564487f
Reshetov Yuriy <45720387+Yurback@users.noreply.github.com>
2024-02-18 22:09:41 +0300

    sink down the lowest one, and retrieve the max

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ class myHeap {
         this.heapElements = [];
     }
 
-    insert (value) {
+    insert(value) {
         this.heapElements.push(value);
         let currentElementIndex = this.heapElements.length - 1;
         let parentElementIndex = Math.floor((currentElementIndex + 1) / 2) - 1;
@@ -15,16 +15,61 @@ class myHeap {
             parentElementIndex = Math.floor((currentElementIndex + 1) / 2) - 1;
         }
     }
+
+    process() {
+        if (this.heapElements.length === 0) {
+            return null;
+        }
+        if (this.heapElements.length === 1) {
+            return this.heapElements.pop();
+        }
+        const topElement = this.heapElements[0];
+
+        this.heapElements[0] = this.heapElements.pop();
+
+        let currentElementIndex = 0;
+        while (currentElementIndex > -1) {
+            let leftChildIndex = 2 * currentElementIndex + 1;
+            let rightChildIndex = 2 * currentElementIndex + 2;
+
+            let childElementIndex = this.heapElements[rightChildIndex] &&
+                this.heapElements[rightChildIndex] >= this.heapElements[leftChildIndex]
+                ? rightChildIndex
+                : leftChildIndex;
+            if (this.heapElements[currentElementIndex] <= this.heapElements[childElementIndex]) {
+                const currentNode = this.heapElements[currentElementIndex];
+                const currentChildNode = this.heapElements[childElementIndex];
+                this.heapElements[childElementIndex] = currentNode;
+                this.heapElements[currentElementIndex] = currentChildNode;
+                currentElementIndex = childElementIndex;
+            } else {
+                currentElementIndex = -1;
+            }
+
+        }
+
+
+
+
+        return topElement;
+    }
 }
 
 const heap = new myHeap();
 
-heap.insert(100);
-heap.insert(120);
+heap.insert(250);
 heap.insert(150);
+heap.insert(120);
+heap.insert(100);
 heap.insert(110);
 heap.insert(10);
-heap.insert(250);
 
+
+// heap.process();
+// heap.process();
+// heap.process();
+// heap.process();
+// heap.process();
+
+console.log(heap.process());
 console.log(heap);
-console.log(Math.floor((0 + 1) / 2) - 1);
